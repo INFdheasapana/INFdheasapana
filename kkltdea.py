3e322a3f1a8c08354e84974eef0681bffcd65969
cchikippop <iniaku00434@gmail.com>
2025-05-15 16:09:20 +0700

    Kalkulator materi

---
+++ kkltdea.py
@@ -0,0 +1,12 @@
+adea = 2
+bdea = 3
+operatordea = '+' #operatornya di input sang peng-input
+
+if (operatordea == '+'):
+    print("nilai = ", adea + bdea)
+elif (operatordea == '-'):
+    print("nilai = ", adea - bdea)
+elif (operatordea == '*'):
+    print("nilai = ", adea * bdea)
+elif (operatordea == '/'):
+    print("niali = ", adea / bdea)
